d123c7b5c0cbdc4a29008b5ec2c43d771fac0bca
Aru <aruscripttips@gmail.com>
2017-06-03 21:53:53 -0700

    Added username and password capabilities

--- Functions/ConnectTo-SQL.ps1
+++ Functions/ConnectTo-SQL.ps1
@@ -2,21 +2,35 @@ function ConnectTo-SQL
 {
     param
     (
-        [Parameter(Mandatory = $false, HelpMessage = "Name of SQL server. Default value is 'localhost'")]
+        [Parameter(Mandatory = $false,ParameterSetName="TrustedConnection",HelpMessage = "Name of SQL server. Default value is 'localhost'")]
+        [Parameter(Mandatory = $false,ParameterSetName="SQLUser",HelpMessage = "Name of SQL server. Default value is 'localhost'")]
         [string]$SQLServer = 'localhost',
-        [Parameter(Mandatory = $false, HelpMessage = "Name of SQL database. Default value is 'master'")]
+        [Parameter(Mandatory = $false,ParameterSetName="TrustedConnection",HelpMessage = "Name of SQL database. Default value is 'master'")]
+        [Parameter(Mandatory = $false,ParameterSetName="SQLUser",HelpMessage = "Name of SQL database. Default value is 'master'")]
         [string]$Database = 'master',
-        [Parameter(Mandatory = $True, HelpMessage = "Query to be executed, avoid using sql comments '--'")]
+        [Parameter(Mandatory = $True,ParameterSetName="TrustedConnection",HelpMessage = "Query to be executed, avoid using sql comments '--'")]
+        [Parameter(Mandatory = $True,ParameterSetName="SQLUser",HelpMessage = "Query to be executed, avoid using sql comments '--'")]
         [string]$SQLQuery = $null,
-        [Parameter(Mandatory = $false, HelpMessage = "Timeout value for query in seconds. Default value is 600")]
+        [Parameter(Mandatory = $false,ParameterSetName="TrustedConnection",HelpMessage = "Timeout value for query in seconds. Default value is 600")]
+        [Parameter(Mandatory = $false,ParameterSetName="SQLUser",HelpMessage = "Timeout value for query in seconds. Default value is 600")]
         [int]$QueryTimeout = 30,
-        [Parameter(Mandatory = $false, HelpMessage = "Turn switch on only if a returned data table is desired")]
-        [switch]$GetTableOutput
+        [Parameter(Mandatory = $false,ParameterSetName="TrustedConnection",HelpMessage = "Turn switch on only if a returned data table is desired")]
+        [Parameter(Mandatory = $false,ParameterSetName="SQLUser",HelpMessage = "Turn switch on only if a returned data table is desired")]
+        [switch]$GetTableOutput,
+        [Parameter(Mandatory = $false,ParameterSetName="SQLUser",HelpMessage = "Specify username if connection requires sql authentication.")]
+        [string]$Username = $null,
+        [Parameter(Mandatory = $true,ParameterSetName="SQLUser",HelpMessage = "Specify Password if connection requires sql authentication.")]
+        [string]$Password = $null
     )
     #Create connection object
     $Connection = New-Object System.Data.SQLClient.SQLConnection
     #build dbconnectionstring
-    $Connection.ConnectionString = "server='$($SQLServer)';database='$($Database)';trusted_connection=true;"
+     switch($PsCmdlet.ParameterSetName)
+     {
+        "TrustedConnection" {$ConnectionString = "server='$($SQLServer)';database='$($Database)';trusted_connection=true;"}
+        "SQLUser"           {$ConnectionString = "server='$($SQLServer)';database='$($Database)';User ID = '$($Username)';Password = '$($Password)';"}
+     }
+    $Connection.ConnectionString = $ConnectionString
     #Open connection
     $Connection.Open()
     #Create command object
