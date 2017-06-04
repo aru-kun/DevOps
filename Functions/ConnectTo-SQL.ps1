f804cc448e2e667c978d8e1fd69213829f109592
aru <aruscripttips@gmail.com>
2017-06-04 00:57:17 -0600

    Added "Query did not return any results" message

--- Functions/ConnectTo-SQL.ps1
+++ Functions/ConnectTo-SQL.ps1
@@ -1,16 +1,20 @@
 function ConnectTo-SQL
 {
+    [CmdletBinding()]
     param
     (
         [Parameter(Mandatory = $false, ParameterSetName = "TrustedConnection", HelpMessage = "Name of SQL server. Default value is 'localhost'")]
         [Parameter(Mandatory = $false, ParameterSetName = "SQLUser", HelpMessage = "Name of SQL server. Default value is 'localhost'")]
+        [ValidatePattern('.+')]
         [Alias('Server', 'ServerName', 'IPAddress')]
         [string]$SQLServer = 'localhost',
         [Parameter(Mandatory = $false, ParameterSetName = "TrustedConnection", HelpMessage = "Name of SQL database. Default value is 'master'")]
         [Parameter(Mandatory = $false, ParameterSetName = "SQLUser", HelpMessage = "Name of SQL database. Default value is 'master'")]
+        [ValidatePattern('.+')]
         [string]$Database = 'master',
         [Parameter(Mandatory = $True, ParameterSetName = "TrustedConnection", HelpMessage = "Query to be executed, avoid using sql comments '--'")]
         [Parameter(Mandatory = $True, ParameterSetName = "SQLUser", HelpMessage = "Query to be executed, avoid using sql comments '--'")]
+        [ValidatePattern('.+')]
         [Alias('Query')]
         [string]$SQLQuery = $null,
         [Parameter(Mandatory = $false, ParameterSetName = "TrustedConnection", HelpMessage = "Timeout value for query in seconds. Default value is 600")]
@@ -24,9 +28,11 @@ function ConnectTo-SQL
         [Parameter(Mandatory = $false, ParameterSetName = "SQLUser", HelpMessage = "Turn switch on only if a returned data table is desired")]
         [switch]$GetTableOutput,
         [Parameter(Mandatory = $false, ParameterSetName = "SQLUser", HelpMessage = "Specify username if connection requires sql authentication.")]
+        [ValidatePattern('.+')]
         [Alias('user')]
         [string]$Username = $null,
         [Parameter(Mandatory = $true, ParameterSetName = "SQLUser", HelpMessage = "Specify Password if connection requires sql authentication.")]
+        [ValidatePattern('.+')]
         [Alias('pwd')]
         [string]$Password = $null
     )
@@ -55,11 +61,18 @@ function ConnectTo-SQL
     #Validate if the GetTableOutput switch is on and if so then it return a table
     if ($GetTableOutput)
     {
-        $Table = new-object System.Data.DataTable
         $Result = $Command.ExecuteReader()
-        $Table.Load($Result)
         $Connection.Close()
-        return $Table
+        if ($Result)
+        {
+            $Table = new-object System.Data.DataTable
+            $Table.Load($Result)
+            return $Table
+        }
+        else
+        {
+            Write-Output "Query did not return any results"
+        }
     }
     else
     {
