c5b33978b076427f51f098d2a2c0d83689437a1d
aru <aruscripttips@gmail.com>
2017-06-03 21:52:01 -0600

    Added a few comments here and there

--- Functions/ConnectTo-SQL.ps1
+++ Functions/ConnectTo-SQL.ps1
@@ -2,25 +2,30 @@ function ConnectTo-SQL
 {
     param
     (
-        [Parameter(Position = 0, Mandatory = $True, HelpMessage = "Name of SQL server")]
-        [string]$SQLServer = $null,
-        [Parameter(Mandatory = $false, HelpMessage = "Name of SQL database")]
+        [Parameter(Mandatory = $false, HelpMessage = "Name of SQL server. Default value is 'localhost'")]
+        [string]$SQLServer = 'localhost',
+        [Parameter(Mandatory = $false, HelpMessage = "Name of SQL database. Default value is 'master'")]
         [string]$Database = 'master',
-        [Parameter(Mandatory = $True, HelpMessage = "Query to be executed, avoid using sql comments '--''")]
+        [Parameter(Mandatory = $True, HelpMessage = "Query to be executed, avoid using sql comments '--'")]
         [string]$SQLQuery = $null,
         [Parameter(Mandatory = $false, HelpMessage = "Timeout value for query in seconds. Default value is 600")]
         [int]$QueryTimeout = 30,
         [Parameter(Mandatory = $false, HelpMessage = "Turn switch on only if a returned data table is desired")]
         [switch]$GetTableOutput
     )
-    #Connect to SQL
+    #Create connection object
     $Connection = New-Object System.Data.SQLClient.SQLConnection
+    #build dbconnectionstring
     $Connection.ConnectionString = "server='$($SQLServer)';database='$($Database)';trusted_connection=true;"
+    #Open connection
     $Connection.Open()
+    #Create command object
     $Command = New-Object System.Data.SQLClient.SQLCommand
+    #Give command all the pieces it needs to run
     $Command.Connection = $Connection
     $Command.CommandText = $SQLQuery
     $Command.CommandTimeout = $QueryTimeout
+    #Validate if the GetTableOutput switch is on and if so then it return a table
     if ($GetTableOutput)
     {
         $Table = new-object System.Data.DataTable
