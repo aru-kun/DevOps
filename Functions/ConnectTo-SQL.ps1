c646a0dca49d463d4c75cd71afe2571ca677d3e4
aru <aruscripttips@gmail.com>
2017-06-03 21:42:00 -0600

    Added $QueryTimeout param and cleaned up syntax a little bit

--- Functions/ConnectTo-SQL.ps1
+++ Functions/ConnectTo-SQL.ps1
@@ -5,27 +5,22 @@ function ConnectTo-SQL
         [Parameter(Position = 0, Mandatory = $True, HelpMessage = "Name of SQL server")]
         [string]$SQLServer = $null,
         [Parameter(Mandatory = $false, HelpMessage = "Name of SQL database")]
-        [string]$Database = $null,
+        [string]$Database = 'master',
         [Parameter(Mandatory = $True, HelpMessage = "Query to be executed, avoid using sql comments '--''")]
         [string]$SQLQuery = $null,
+        [Parameter(Mandatory = $false, HelpMessage = "Timeout value for query in seconds. Default value is 600")]
+        [int]$QueryTimeout = 30,
         [Parameter(Mandatory = $false, HelpMessage = "Turn switch on only if a returned data table is desired")]
         [switch]$GetTableOutput
     )
     #Connect to SQL
     $Connection = New-Object System.Data.SQLClient.SQLConnection
-    if ($Database)
-    {
-        $Connection.ConnectionString = "server='$($SQLServer)';database='$($Database)';trusted_connection=true;"
-    }
-    else
-    {
-        $Connection.ConnectionString = "server='$($SQLServer)';database='master';trusted_connection=true;"
-    }
+    $Connection.ConnectionString = "server='$($SQLServer)';database='$($Database)';trusted_connection=true;"
     $Connection.Open()
     $Command = New-Object System.Data.SQLClient.SQLCommand
     $Command.Connection = $Connection
     $Command.CommandText = $SQLQuery
-    $Command.CommandTimeout = 600
+    $Command.CommandTimeout = $QueryTimeout
     if ($GetTableOutput)
     {
         $Table = new-object System.Data.DataTable
